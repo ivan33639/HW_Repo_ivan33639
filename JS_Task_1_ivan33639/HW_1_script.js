fa3b78d48962a23388ac43e09f9c2d602fc86c36
ivan33639 <ivan33639@gmail.com>
2023-02-12 22:10:11 +0200

    JS_1_ivan33639

---
+++ JS_Task_1_ivan33639/HW_1_script.js
@@ -0,0 +1,14 @@
+//1. In this task you have to declare four variables: a = 0,1; b = 0,7; c = 0,2; d = 0,8;
+
+var a = 0.1; 
+var b = 0.7; 
+var c = 0.2; 
+var d = 0.8;
+
+//2. After that, console.log the following mathematical operations:
+
+console.log(a + b); console.log(a + a + d); console.log(c + d);
+
+//3. Analyse the results and record any mathematical discrepancies.
+
+// console.log of (a + b) shows '0.7999999999999999'- it's not rounded, the other cases seems correct.
